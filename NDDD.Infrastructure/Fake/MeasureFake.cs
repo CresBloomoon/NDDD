b57e052809fed0762b8b1f7fe87a30cfc3d1da29
マサキ カタオカ <metroidm2421@gmail.com>
2022-07-30 19:40:12 +0900

    Fakeデータを外部から読み込めるようにしました

--- NDDD.Infrastructure/Fake/MeasureFake.cs
+++ NDDD.Infrastructure/Fake/MeasureFake.cs
@@ -6,12 +6,27 @@ namespace NDDD.Infrastructure.Fake
 {
     internal sealed class MeasureFake : IMeasureRepository
     {
+        internal const string FakePath = @"C:\NDDDFake\";
         public MeasureEntity GetLatest()
         {
-            return new MeasureEntity(
-                10,
-                Convert.ToDateTime("2020/12/12 12:34:56"),
-                123.341f);
+            try
+            {
+                var lines = System.IO.File.ReadAllLines(FakePath + "MeasureFake.csv");
+                var value = lines[0].Split(',');
+                return new MeasureEntity(
+                    Convert.ToInt32(value[0]),
+                    Convert.ToDateTime(value[1]),
+                    Convert.ToSingle(value[2]));
+            }
+            catch
+            {
+                return new MeasureEntity(
+                    10,
+                    Convert.ToDateTime("2020/12/12 12:34:56"),
+                    123.341f);
+            }
+
+
         }
     }
 }
