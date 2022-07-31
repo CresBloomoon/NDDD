9e4e1a068797c1aef7996a8b9ca0f2bcf3d4bff0
マサキ カタオカ <metroidm2421@gmail.com>
2022-07-31 18:23:40 +0900

    Logicsを作成しました 59

---
+++ NDDD.Domain/Logics/MeasureAnalysis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDD.Domain.Logics
+{
+    /// <summary>
+    /// EntitiesやValueObjectsに入らないような
+    /// 共通ロジック置き場
+    /// </summary>
+    public static class MeasureAnalysis
+    {
+        public static float Execute(
+            float value1,
+            float value2,
+            float value3,
+            float value4,
+            float value5)
+        {
+            return 999f;
+        }
+    }
+}
